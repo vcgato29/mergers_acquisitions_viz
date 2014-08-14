0fa5fa61c5c4a7cff53191e246fe92cd478a9756
sjiang <jshangmin2011@gmail.com>
2014-08-13 20:27:11 -0400

    add grouped bar charts

--- vis.js
+++ vis.js
@@ -18,6 +18,10 @@ var vis = function(data){
 	})()
 
 	var color_pool = ['rgb(166,206,227)','rgb(31,120,180)','rgb(178,223,138)','rgb(51,160,44)','rgb(251,154,153)','rgb(227,26,28)','rgb(253,191,111)','rgb(255,127,0)','rgb(202,178,214)', 'rgb(153, 153, 153)']
+	var bar_colors = {'Executive/Board Changes - Other': 'rgb(179,226,205)',
+					  'Lawsuits & Legal Issues': 'rgb(253,205,172)',
+					  'Business Expansions': 'rgb(203,213,232)'
+					 }
 	var colors = {
 		ENSG: color_pool[0],
 		LHCG: color_pool[1],
@@ -37,6 +41,7 @@ var vis = function(data){
 
 		var render = function() {
 			pieChart.plot();
+			groupBarChart.plot();
 		};
 		controller.init = function() {
 			render();
@@ -47,7 +52,6 @@ var vis = function(data){
 
 
 	var data = (function() {
-
 		data.byTransactions = d3.nest().key(function(d) {
 			return d['Buyers/Investors']
 		}).entries(data.transactions);
@@ -58,6 +62,24 @@ var vis = function(data){
 				volume: d.values.length
 			}
 		});
+		data.byType = d3.nest().key(function(d) { return d['Company Name(s)'] })
+							   .key(function(d) { return d['Key Developments by Type']})
+							   .rollup(function(leaves) { return leaves.length; })
+							   .map(data.text, d3.map);
+
+		data.groupBarData = []
+		data.types = ['Executive/Board Changes - Other', 'Lawsuits & Legal Issues', 'Business Expansions'];
+		//data.company_short_names = [];
+		for (var c_name in data.byType) {
+			//company_short_names.push(getShortName(c_name));
+			data.groupBarData.push({
+				name: getShortName(c_name),
+				company_name: c_name,
+				activities: data.types.map(function(t) {
+					return {type: t, value: data.byType[c_name][t] || 0}
+				})
+			})
+		}
 		data.byText = d3.nest().key(function(d) {
 			return d['Company Name(s)']
 		}).entries(data.text);
@@ -77,10 +99,10 @@ var vis = function(data){
 		return data
 	})();
 
-	console.log(data)
+	console.log('data', data)
 
 	var pieChart = (function() {
-		pieChart = {};
+		var pieChart = {};
 		var pieWidth = 250;
 		var pieHeight = 300;
 		var radius = Math.min(pieWidth, pieHeight) / 2;
@@ -141,7 +163,6 @@ var vis = function(data){
 						.style('background-color', null)
 						.style('color', 'black');
 				});
-		
 		//draw table
 		(function() {
 			var table = d3.select('#pie-wrapper')
@@ -191,13 +212,85 @@ var vis = function(data){
 				.append('td')
 				.text(function(d) { return d.value; })
 
-
-
-
 		})()
 		}
-
 		return pieChart;
+	})()
+
+	var groupBarChart = (function() {
+		var groupBarChart = {};
+		var margin = {
+			top: 40,
+			right: 40,
+			bottom: 30,
+			left: 40
+			}
+		var barWidth = 700 - margin.left - margin.right;
+		var barHeight = 400 - margin.top - margin.bottom;
+		var x0 = d3.scale.ordinal()
+				.rangeRoundBands([0, barWidth], 0.1);
+		var	x1 = d3.scale.ordinal();
+		var y = d3.scale.linear().range([barHeight, 0]);
+		var xAxis = d3.svg.axis()
+			.scale(x0)
+			.orient('bottom');
+		var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient('left');
+		var groupBarChartSVG = d3.select('svg.barchart')
+			.attr('width', barWidth + margin.left + margin.right)
+			.attr('height', barHeight + margin.top + margin.bottom)
+			.append('g')
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+		groupBarChart.plot = function() {
+			x0.domain(data.groupBarData.map(function(d) { return d.name; }));
+			x1.domain(data.types).rangeRoundBands([0, x0.rangeBand()]);
+			y.domain([0, d3.max(data.groupBarData, function(d) {
+				return d3.max(d.activities, function(a) {
+					return a.value;
+				})
+			})])
+			
+			groupBarChartSVG.append('g')
+				.attr('class', 'x axis')
+				.attr('transform', 'translate(0,' + barHeight + ')')
+				.call(xAxis);
+			groupBarChartSVG.append('g')
+				.attr('class', 'y axis')
+				.call(yAxis)
+				.append('text')
+				.attr('transform', 'rotate(-90)')
+				.attr('y', 6)
+				.attr('dy', '.71em')
+				.style('text-anchor', 'end')
+				.text('#activities');
+
+			var company = groupBarChartSVG.selectAll('.company')
+				.data(data.groupBarData)
+				.enter()
+				.append('g')
+				.attr('class', 'company activities')
+				.attr('transform', function(d) { return 'translate(' + x0(d.name) + ',0)'; });
+			company.selectAll('rect')
+				.data(function(d) { return d.activities; })
+				.enter()
+				.append('rect')
+				.attr('width', x1.rangeBand())
+				.attr('x', function(d) { return x1(d.type); })
+				.attr('y', function(d) { return y(d.value); })
+				.attr('height', function(d) { return barHeight - y(d.value); })
+				.style('fill', function(d, i) { return bar_colors[d.type]; })
+
+
+
+		}
+
+
+
+
+
+		return groupBarChart;
 	})()
 
 	controller.init()
